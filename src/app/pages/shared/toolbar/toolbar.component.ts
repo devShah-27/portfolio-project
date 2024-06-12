f6b23dc2dba9b61d1ed3fea3ead53940bb6d0efe
Dev Shah <dev.d@royalecheese.com>
2024-06-12 07:29:52 +0530

    feat: change layout of page

--- src/app/pages/shared/toolbar/toolbar.component.ts
+++ src/app/pages/shared/toolbar/toolbar.component.ts
@@ -1,14 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [ToolbarModule, ButtonModule],
+  imports: [
+    ToolbarModule,
+    ButtonModule,
+    RouterLink,
+    RouterLinkActive,
+    DividerModule,
+  ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
 })
 export class ToolbarComponent {
   @Input() pageTitle: string = 'Home';
+
+  sideBarStatus: boolean = false;
+
+  toggleSidebar() {
+    this.sideBarStatus=!this.sideBarStatus
+  }
 }
